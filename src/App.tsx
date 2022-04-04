d047dac33437e5fa14aa7a6d1aa57aa53a38f086
PC-HoangTC <PC-HoangTC@DESKTOP-8F979J0>
2022-04-04 07:52:52 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <h1>{process.env.NODE_ENV} -  {process.env.name}</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
